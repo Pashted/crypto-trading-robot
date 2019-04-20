9a0dd166a196cc4209900e6ebffa92d28c63508a
Pashted <pashted9146@gmail.com>
2019-04-20 21:22:35 +0300

    Integrate public js files into webpack bundle

--- routes/index.js
+++ routes/index.js
@@ -14,33 +14,6 @@ router.get('/', async (req, res, next) => {
     }
 );
 
-router.get('/jquery.js', (req, res, next) =>
-    fs.readFile("./node_modules/jquery/dist/jquery.js", 'utf8', (err, data) => {
-        if (err) throw err;
-        res.end(data);
-    })
-);
-
-router.get('/uikit.js', (req, res, next) =>
-    fs.readFile("./node_modules/uikit/dist/js/uikit.js", 'utf8', (err, data) => {
-        if (err) throw err;
-        res.end(data);
-    })
-);
-
-router.get('/uikit-icons.js', (req, res, next) =>
-    fs.readFile("./node_modules/uikit/dist/js/uikit-icons.js", 'utf8', (err, data) => {
-        if (err) throw err;
-        res.end(data);
-    })
-);
-
-router.get('/chosen.js', (req, res, next) =>
-    fs.readFile("./node_modules/chosenjs/chosen.jquery.js", 'utf8', (err, data) => {
-        if (err) throw err;
-        res.end(data);
-    })
-);
 router.get('/stylesheets/chosen-sprite.png', (req, res, next) => {
         let img = fs.readFileSync('./node_modules/chosenjs/chosen-sprite.png');
         res.writeHead(200, { 'Content-Type': 'image/gif' });
