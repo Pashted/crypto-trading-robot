1628bfe73768e053ed32622bac91a03a7ed72432
Pashted <pashted9146@gmail.com>
2019-04-22 03:43:17 +0300

    Move exchange symbols settings into separate module

--- public/javascripts/src/pages/settings.js
+++ public/javascripts/src/pages/settings.js
@@ -2,6 +2,9 @@ import { initMain } from '../main';
 
 import { serializeJSON as serialize } from '../modules/serializeJSON';
 
+import * as Symbols from '../modules/exchangeSettings';
+
+
 initMain().then(() => {
 
     console.log('>> RUN_ settings.js');
@@ -25,73 +28,11 @@ initMain().then(() => {
     });
 
     let form = $('.settings-form'),
-
-        syms = form.find('#symbol'),
-        pairs = form.find('#pair'),
-
-        selected_pair = form.find('#selected-pair'),
-
-        last_pair = pairs.data('value'),
-
         section_icon = $('aside .uk-active [uk-icon]').attr('uk-icon'),
         note_timeout = 3,
         note_style = 'success';
 
-    syms.on({
-        change() {
-
-            pairs.html('');
-
-            let opt = syms.trigger('chosen:updated').find('option').eq(syms[0].selectedIndex);
-            if (!opt.length)
-                return false;
-
-            let values = opt.data('pairs').split(',');
-
-            $.each(values, (i, e) => {
-                pairs.append(`<option value="${e}">${e}</option>`)
-            });
-
-            if ($.inArray(last_pair, values) < 0)
-                last_pair = pairs.data('default');
-
-            pairs.val(last_pair)
-                .trigger('change');
-        }
-    });
-
-    pairs.on({
-        change() {
-            last_pair = pairs.trigger('chosen:updated').val();
-
-            if (!selected_pair.is(':focus'))
-                selected_pair.val(syms.val() + last_pair);
-        }
-    });
-
-    syms.trigger('change');
-
-    selected_pair.on({
-        input() {
-            let val = selected_pair.val(),
-
-                cur = val.substr(0, 3), // first currency in pair
-                pair = val.substr(3); // second currency in pair
-
-            if (syms.find(`option[value="${cur}"]`).length)
-                syms.val(cur)
-                    .trigger('change');
-
-            if (pairs.find(`option[value="${pair}"]`).length)
-                pairs.val(pair)
-                    .trigger('change');
-
-        },
-        blur() {
-            pairs.trigger('change')
-        }
-    });
-
+    Symbols.init();
 
     form.find('button.save').click(function () {
 
