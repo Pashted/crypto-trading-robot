3e9d3bb05bccdbe9e55890486d41a0011ffda418
Pashted <pashted9146@gmail.com>
2019-04-28 16:09:02 +0300

    Show additional message in console after db connection

--- model/database/mongodb/index.js
+++ model/database/mongodb/index.js
@@ -18,6 +18,7 @@ module.exports = {
             resolve();
 
             console.log(">> DB CONNECTED successfully");
+            console.log(">> Go to http://localhost:3000");
         })
     }),
 
